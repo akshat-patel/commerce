85069da9a35a727e1d29ebd63a134e72e3dda177
ghostraddish <akshat365@gmail.com>
2020-07-15 15:31:33 -0700

    Add User, Bic, Comment, and Auction models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Bid, Auction, Comment
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Bid)
+admin.site.register(Auction)
+admin.site.register(Comment)
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,31 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from datetime import datetime
+from django.core.validators import MinValueValidator
 
 class User(AbstractUser):
     pass
+
+class Bid(models.Model):
+    bidValue = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0),])
+
+    def __str__(self):
+        return f"{self.bidValue} bid"
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=500, default="")
+    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE, related_name="comments", default=None)
+
+    def __str__(self):
+        return f"{self.user}: {self.comment}"
+
+class Auction(models.Model):
+    name = models.CharField(max_length=64,default="")
+    price = models.PositiveIntegerField(validators=[MinValueValidator(0),])
+    description = models.CharField(max_length=200, blank=True, default="")
+    dateTime = models.DateTimeField(auto_now=True)
+    bid = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name="auctions", default=price)
+    comment = models.ForeignKey(Comment, on_delete=models.DO_NOTHING, default=None)
+
+    def __str__(self):
+        return f"{self.name}: ${self.price}, {self.description}, {self.dateTime}, ${self.bid}"
