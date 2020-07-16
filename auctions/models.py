5c8a1c29cfcb19b7c60ac86bfd748f8d5816f9ae
ghostraddish <akshat365@gmail.com>
2020-07-16 13:55:43 -0700

    Change Comment model to have listing data

--- auctions/models.py
+++ auctions/models.py
@@ -12,21 +12,21 @@ class Bid(models.Model):
     def __str__(self):
         return f"{self.bidValue} bid"
 
-class Comment(models.Model):
-    comment = models.CharField(max_length=500, default="")
-    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE, related_name="comments", default=None)
-
-    def __str__(self):
-        return f"{self.user}: {self.comment}"
-
 class Auction(models.Model):
     name = models.CharField(max_length=64,default="")
     bid = models.PositiveIntegerField(validators=[MinValueValidator(0),])
     description = models.CharField(max_length=200, blank=True, default="")
     dateTime = models.DateTimeField(auto_now=True)
-    comment = models.ForeignKey(Comment, on_delete=models.DO_NOTHING, blank=True, null=True, related_name="auctions")
-    url = models.URLField(max_length=300, default="#", blank=True)
+    url = models.URLField(max_length=300, default="", blank=True)
     category=models.CharField(max_length=50, default="")
 
     def __str__(self):
-        return f"{self.name}: {self.description}, {self.dateTime}, ${self.bid}, {self.url}, {self.category}"
+        return f"{self.name}"
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=500, default="")
+    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE, related_name="comments", default=None)
+    listing = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user} on {self.listing}: {self.comment}"
